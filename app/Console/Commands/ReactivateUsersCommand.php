1b560535c3c6823542cec6be899df20d29ab63c4
Hassan Javaid <hassanjrao@gmail.com>
2024-09-10 09:27:07 +0400

    added account reactive mail

--- app/Console/Commands/ReactivateUsersCommand.php
+++ app/Console/Commands/ReactivateUsersCommand.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\User;
 use App\Notifications\ReactiveUserNotification;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class ReactivateUsersCommand extends Command
 {
@@ -51,6 +52,11 @@ class ReactivateUsersCommand extends Command
             $user->save();
 
             $user->notify(new ReactiveUserNotification($user));
+
+            Log::info('ReactivateUsersCommand',[
+                'user_id' => $user->id,
+                'email' => $user->email
+            ]);
         }
 
         $this->info('Users reactivated successfully');
