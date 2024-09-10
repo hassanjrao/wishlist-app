0e114042a8e384369763ef75239808ffc235aa71
Hassan Javaid <hassanjrao@gmail.com>
2024-09-10 08:58:28 +0400

    added verified check

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -65,7 +65,7 @@
                                     </div>
                                 </div>
 
-                                <div class="row mb-0">
+                                <div class="row mb-3">
                                     <div class="col-md-6 offset-md-4">
                                         <button type="submit" class="btn btn-primary">
                                             {{ __('Reset Password') }}
