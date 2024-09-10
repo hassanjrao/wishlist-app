408a9a277f1be898e57c0eab4c127d8a283550bc
Hassan Javaid <hassanjrao@gmail.com>
2024-09-10 09:24:38 +0400

    added account reactive mail

---
+++ app/Console/Commands/ReactivateUsersCommand.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use App\Notifications\ReactiveUserNotification;
+use Illuminate\Console\Command;
+
+class ReactivateUsersCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'users:reactivate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->info('Reactivating users...');
+        $users = User::where('is_approved', 0)
+        ->whereHas('roles', function ($q) {
+            $q->where('name', 'user');
+        })
+        ->get();
+
+        foreach ($users as $user) {
+            $user->is_approved = 1;
+            $user->save();
+
+            $user->notify(new ReactiveUserNotification($user));
+        }
+
+        $this->info('Users reactivated successfully');
+    }
+}
---
+++ app/Notifications/ReactiveUserNotification.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ReactiveUserNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public $user;
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->greeting('Hello, '.$this->user->name)
+                    ->subject('Your account has been reactivated')
+                    ->line('We are happy to inform you that your account has been reactivated upon the anniversary of your account registration.')
+                    ->action('View WishLists', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
